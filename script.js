964b8e01df9322c21599332d5cbb5b0efeb81c57
RabeloGuedes <krepscredian@gmail.com>
2022-02-15 17:00:36 -0300

    refact: Modifica a lógica que cria os pixels, fazendo a ficar mais simples.

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 // Variables declaration
 const paletaDeCores = ['black', 'red', 'blue', 'green'];
 const colors = document.querySelectorAll('.color');
-const pixelLines = 5;
-const pixelColumns = 5;
+const pixelLinesColumns = 5;
 const pixelBoard = document.querySelector('#pixel-board');
+let selected = document.querySelector('.color');
+let selectedColor = getComputedStyle(selected).backgroundColor;
 // Variables declaration
 for (let index = 0; index < paletaDeCores.length; index += 1) {
   colors[index].style.backgroundColor = paletaDeCores[index];
 }
 
 function pixels() {
-  for (let index = 0; index < pixelLines; index += 1) {
+  for (let index = 0; index < pixelLinesColumns; index += 1) {
     const pixelLine = document.createElement('div');
     pixelLine.className = 'pixel-line';
     pixelBoard.appendChild(pixelLine);
-    for (let index2 = 0; index2 < pixelColumns; index2 += 1) {
+    for (let index2 = 0; index2 < pixelLinesColumns; index2 += 1) {
       const singlePixel = document.createElement('div');
       singlePixel.className = 'pixel';
       pixelLine.appendChild(singlePixel);
@@ -22,3 +23,5 @@ function pixels() {
   }
 }
 pixels();
+
+selected.className = 'color selected';
