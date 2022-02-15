e2eae640f9a91a23c7a4f7aa5fc83a735f1475c0
RabeloGuedes <krepscredian@gmail.com>
2022-02-15 15:07:20 -0300

    feat: Configura ambiente para desenvolvimento, criando arquivos base, index.html, style.css, script.js.

---
+++ script.js
@@ -0,0 +1,7 @@
+// Variables declaration
+const paletaDeCores = ['black', 'red', 'blue', 'green'];
+const colors = document.querySelectorAll('.color');
+// Variables declaration
+for (let index = 0; index < paletaDeCores.length; index += 1) {
+  colors[index].stlye.backgroundColor = paletaDeCores[index];
+}
