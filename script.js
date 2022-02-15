bf91d451784648b8804c11e1165a8403054f2d5b
RabeloGuedes <krepscredian@gmail.com>
2022-02-15 15:18:01 -0300

    fix: Modifica o 'id' da tag section, corrigi erro de digitação lógica do laço de repetição do arquivo 'script.js' e padroniza o tamanho e centraliza as palhetas de cores.

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ const paletaDeCores = ['black', 'red', 'blue', 'green'];
 const colors = document.querySelectorAll('.color');
 // Variables declaration
 for (let index = 0; index < paletaDeCores.length; index += 1) {
-  colors[index].stlye.backgroundColor = paletaDeCores[index];
+  colors[index].style.backgroundColor = paletaDeCores[index];
 }
